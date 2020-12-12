c5dc8d76cd13745acf8d8fe9d53ef819b6f4885e
Parth Pandey <parthpandey2236@gmail.com>
2020-12-12 18:13:11 +0530

    ProfilePicUpdation-1

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:saferx/authentication.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -14,164 +15,164 @@ class profile extends StatefulWidget {
 }
 
 class _profileState extends State<profile> {
-  // File _image;
-  // final picker=ImagePicker();
-  // String CircleAvtarLink=null;
-  // CollectionReference UserRefrance = FirebaseFirestore.instance.collection('ProfilePicUrl');
+  File _image;
+  final picker=ImagePicker();
+  String CircleAvtarLink=null;
+  CollectionReference UserRefrance = FirebaseFirestore.instance.collection('ProfilePicUrl');
 
-  // Future<void> AddToFirestore(var url){
-  //   return UserRefrance.doc(email.substring(0,email.indexOf('@'))).set({
-  //     'URL' : url,
-  //   }).then((value)=>print('user added'))
-  //       .catchError((error)=> print('Failed to add User'));
-  // }
+  Future<void> AddToFirestore(var url){
+    return UserRefrance.doc(email.substring(0,email.indexOf('@'))).set({
+      'URL' : url,
+    }).then((value)=>print('user added'))
+        .catchError((error)=> print('Failed to add User'));
+  }
 
-  // Future<firebase_storage.UploadTask> uploadFile(BuildContext context) async{
-  //   String fileName=path.basename(_image.path);
-  //   firebase_storage.Reference ref= firebase_storage.FirebaseStorage.instance.ref().child(email.substring(0,email.indexOf('@'))).child(fileName);
-  //   firebase_storage.UploadTask uploadTask = ref.putFile(_image);
-  //   final url1=await (await uploadTask).ref.getDownloadURL();
-  //   //print(url1.toString());
-  //   setState(() {
-  //     CircleAvtarLink =url1.toString();
-  //     AddToFirestore(url1);
-  //   });
-  // }
+  Future<StorageUploadTask> uploadFile(BuildContext context) async{
+    String fileName=path.basename(_image.path);
+    StorageReference ref= firebase_storage.FirebaseStorage.instance.ref().child(email.substring(0,email.indexOf('@'))).child(fileName);
+    StorageUploadTask uploadTask = ref.putFile(_image);
+    StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
+    final url1=await taskSnapshot.ref.getDownloadURL();
+    //print(url1.toString());
+    setState(() {
+      CircleAvtarLink =url1.toString();
+      AddToFirestore(url1);
+    });
+  }
 
-  // Future<void> getImageViaGallery() async{
-  //   Navigator.pop(context);
-  //   final pickedFile =await picker.getImage(source: ImageSource.gallery);
-  //   if(pickedFile!=null){
-  //     final croppedFile=await ImageCropper.cropImage(
-  //       sourcePath: File(pickedFile.path).path,
-  //     );
-  //     setState(() {
-  //       if(croppedFile!=null){
-  //         _image=File(croppedFile.path);
-  //         uploadFile(context);
+  Future<void> getImageViaGallery() async{
+    Navigator.pop(context);
+    final pickedFile =await picker.getImage(source: ImageSource.gallery);
+    if(pickedFile!=null){
+      final croppedFile=await ImageCropper.cropImage(
+        sourcePath: File(pickedFile.path).path,
+      );
+      setState(() {
+        if(croppedFile!=null){
+          _image=File(croppedFile.path);
+          uploadFile(context);
 
-  //       }else{
-  //         print('No file selected');
-  //       }
-  //     });
-  //   }
-  // }
+        }else{
+          print('No file selected');
+        }
+      });
+    }
+  }
 
-  // Future<void> getImageViaCamera() async{
-  //   Navigator.pop(context);
-  //   final pickedFile =await picker.getImage(source: ImageSource.camera);
-  //   if(pickedFile!=null){
-  //     final croppedFile=await ImageCropper.cropImage(
-  //       sourcePath: File(pickedFile.path).path,
-  //     );
-  //     setState(() {
-  //       if(croppedFile!=null){
+  Future<void> getImageViaCamera() async{
+    Navigator.pop(context);
+    final pickedFile =await picker.getImage(source: ImageSource.camera);
+    if(pickedFile!=null){
+      final croppedFile=await ImageCropper.cropImage(
+        sourcePath: File(pickedFile.path).path,
+      );
+      setState(() {
+        if(croppedFile!=null){
 
-  //         _image=File(croppedFile.path);
-  //         uploadFile(context);
+          _image=File(croppedFile.path);
+          uploadFile(context);
 
-  //       }else{
-  //         print('No file selected');
-  //       }
-  //     });
-  //   }
-  //   else{
-  //     print('No file selected');
-  //   }
-  // }
+        }else{
+          print('No file selected');
+        }
+      });
+    }
+    else{
+      print('No file selected');
+    }
+  }
 
-  // String setImage(){
-  //   String mainLink=null;
-  //   UserRefrance.doc(email.substring(0,email.indexOf('@')))
-  //       .get()
-  //       .then((DocumentSnapshot documentSnapshot) {
-  //     if (documentSnapshot.exists) {
-  //       var link=documentSnapshot.data()['URL'];
-  //       print(link);
-  //       setState(() {
-  //         CircleAvtarLink=link.toString();
-  //       });
-  //       //  CircleAvtarImage=link.toString();
-  //       print(CircleAvtarLink);
-  //     }
-  //     else {
-  //       print('unsucsessful');
-  //     }});
+  String setImage(){
+    String mainLink=null;
+    UserRefrance.doc(email.substring(0,email.indexOf('@')))
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        var link=documentSnapshot.data()['URL'];
+        print(link);
+        setState(() {
+          CircleAvtarLink=link.toString();
+        });
+        //  CircleAvtarImage=link.toString();
+        print(CircleAvtarLink);
+      }
+      else {
+        print('unsucsessful');
+      }});
 
-  // }
+  }
 
-  // void displayBottomSheet(){
-  //   showModalBottomSheet(
-  //       context: context,
-  //       builder: (context){
-  //         return Container(
-  //           color: Color(0xFF737373),
-  //           height: 150,
-  //           child: Container(
-  //             decoration: BoxDecoration(
-  //                 color: Theme.of(context).canvasColor,
-  //                 borderRadius: BorderRadius.only(
-  //                     topLeft: Radius.circular(15),
-  //                     topRight: Radius.circular(15)
-  //                 )
-  //             ),
-  //             child: Column(
-  //               children: [
-  //                 ListTile(
-  //                   leading: Icon(Icons.camera),
-  //                   title: Text('Camera'),
-  //                   onTap: getImageViaCamera,
-  //                 ),
-  //                 ListTile(
-  //                   leading: Icon(Icons.photo),
-  //                   title: Text('Gallery'),
-  //                   onTap: getImageViaGallery,
-  //                 )
-  //               ],
+  void displayBottomSheet(){
+    showModalBottomSheet(
+        context: context,
+        builder: (context){
+          return Container(
+            color: Color(0xFF737373),
+            height: 150,
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)
+                  )
+              ),
+              child: Column(
+                children: [
+                  ListTile(
+                    leading: Icon(Icons.camera),
+                    title: Text('Camera'),
+                    onTap: getImageViaCamera,
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.photo),
+                    title: Text('Gallery'),
+                    onTap: getImageViaGallery,
+                  )
+                ],
 
-  //             ),
-  //           ),
-  //         );
-  //       }
-  //   );
-  // }
-  // @override
-  // void initState() {
-  //   // TODO: implement initState
-  //   setImage();
-  //   setState(() {});
-  //   super.initState();
-  // }
+              ),
+            ),
+          );
+        }
+    );
+  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    setImage();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // body: SafeArea(
+        body: SafeArea(
 
-        //   child: Center(
-        //     child: Column(
-        //       children: [
-        //         CircleAvatar(
-        //           radius: 50.0,
-        //           backgroundImage: CircleAvtarLink==null?AssetImage('images/profile.png'):NetworkImage(CircleAvtarLink),
-        //         ),
-        //         FlatButton(
-        //           child: Icon(Icons.add_a_photo),
-        //           onPressed: displayBottomSheet,
-        //         ),
-        //         FlatButton(
-        //           color: Colors.purple,
-        //           child: Text('LogOut'),
-        //           onPressed: () async {
-        //             final prefs = await SharedPreferences.getInstance();
-        //             prefs.remove('seferX_email');
-        //             Navigator.push(context,
-        //                 MaterialPageRoute(builder: (context) => Authentication()));
-        //           },
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
+          child: Center(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundImage: CircleAvtarLink==null?AssetImage('images/profile.png'):NetworkImage(CircleAvtarLink),
+                ),
+                FlatButton(
+                  child: Icon(Icons.add_a_photo),
+                  onPressed: displayBottomSheet,
+                ),
+                FlatButton(
+                  color: Colors.purple,
+                  child: Text('LogOut'),
+                  onPressed: () async {
+                    final prefs = await SharedPreferences.getInstance();
+                    prefs.remove('seferX_email');
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Authentication()));
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
 
         );
   }
